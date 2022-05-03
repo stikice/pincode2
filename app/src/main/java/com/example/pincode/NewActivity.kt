e1708ef00628339ab52288a78ac6d795e48ee0d2
Adlet Zhaimukhamedov <stikice2014@gmail.com>
2022-05-04 00:27:20 +0600

    first commit

---
+++ app/src/main/java/com/example/pincode/NewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pincode
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class NewActivity: AppCompatActivity(R.layout.activity_new) {
+
+}
